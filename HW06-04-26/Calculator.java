4b1f792718c01f51d5e02448c1c59eff4cac9462
ArunaChandrasekaran <arunadevitvr@gmail.com>
2026-04-06 18:27:14 +0530

    april 6,2026 - homework

---
+++ HW06-04-26/Calculator.java
@@ -0,0 +1,13 @@
+class Calculator{
+	public static void main(String args [])
+	{
+		int a = 5;
+		int b = 10;
+		
+		System.out.println("Addition of a,b : "+ (a+b));
+		System.out.println("Subtraction of a,b : "+ (a-b));
+	    System.out.println("Multiplication of a,b : "+ (a*b));
+		System.out.println("Division of a,b : "+ (a/b));
+		System.out.println("Modulus of a,b : "+ (a%b));
+	}
+}

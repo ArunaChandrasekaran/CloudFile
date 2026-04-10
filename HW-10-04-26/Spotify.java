3aa373419b82696f7006fbcb20c3a833eb96594f
ArunaChandrasekaran <arunadevitvr@gmail.com>
2026-04-10 19:37:25 +0530

    April 10,2026. HOMEWORK

---
+++ HW-10-04-26/Spotify.java
@@ -0,0 +1,681 @@
+import java.util.Scanner;
+class Spotify{
+	public static void main(String args[])
+	{
+		
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("***** SPOTIFY WELCOMES YOU *******");
+		System.out.println("***** SELECT YOUR OPTION, THE OPTIONS ARE *******");
+
+		System.out.println("1.A.R. RAHMAN");
+		System.out.println("2.ANIRUDH");
+		System.out.println("3.HARRIS JEYARAJ");
+		System.out.println("4.ILLAIYARAJA");
+		System.out.println("5.U1");
+		
+		System.out.print("CHOOSE ANY ONE OF THE SINGER: ");
+		
+		int singer = sc.nextInt();
+		
+		if(singer == 1) //1. A.R. RAHMAN
+		{
+			System.out.println("1.MELODY");
+			System.out.println("2.KUTHU");
+			System.out.println("3.MOTIVATIONAL SONG");
+			System.out.println("4.SAD");
+			System.out.print("CHOOSE ANY ONE OF THE OPTIONS: ");
+			
+			int options = sc.nextInt();
+			
+			if(options == 1)
+			{
+				System.out.println("1.NETRU ILLADHA MATRAM");
+				System.out.println("2.ENNA SOLLA POGIRAI");
+				System.out.println("3.ENNAVALEY ADI ENNAVALEY");
+				System.out.println("4.MALARGALEY MALARGALEY");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST : ");
+				
+				int play = sc.nextInt();
+				
+				if(play == 1)
+				{
+					System.out.println("NETRU ILLADHA MATRAM....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("ENNA SOLLA POGIRAI....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("ENNAVALEY ADI ENNAVALEY....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("MALARGALEY....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+				
+			}
+			
+			else if(options == 2)
+			{
+				System.out.println("1.THAIYA THAIYA");
+				System.out.println("2.MUKKALA MUKKABALA");
+				System.out.println("3.URVASHI URVASHI");
+				System.out.println("4.AATHICHUDI");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("THAIYA THAIYA....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("MUKKALA MUKKABALA....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("URVASHI URVASHI....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("AATHICHUDI....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			}
+			else if(options == 3)
+			{
+				System.out.println("1.VANDEY MATARAM");
+				System.out.println("2.ORUVAN ORUVAN MUDHALALI");
+				System.out.println("3.SHAKTHI KODU");
+				System.out.println("4.KAPPAL YEARI POYACHU");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("VANDHEY MATARAM....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("ORUVAN ORUVAN MUDHALALI....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("SHAKTHI KODU....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("KAPPAL YEARI POYACHU....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+				
+			}
+			else if(options == 4)
+			{
+				System.out.println("1.POONGATRILE");
+				System.out.println("2.MARUDAANI");
+				System.out.println("3.MINNALEA EN VAZHVIN");
+				System.out.println("4.RASATHI");
+				System.out.println("5.ORU POIYAVATHU SOL");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST: ");
+					int play = sc.nextInt();
+		
+					
+				if(play == 1)
+				{
+					System.out.println("POONGATRILE....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("MARUDAANI....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("MINNALEA EN VAZHVIN....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("RASATHI....");
+				}
+				else if(play == 5)
+				{
+					System.out.println("ORU POIYATHU SOL....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			}
+		}
+		
+		else if(singer == 2) // ANIRUDH
+		{
+			System.out.println("1.MELODY");
+			System.out.println("2.KUTHU");
+			System.out.println("3.MOTIVATIONAL SONG");
+			System.out.print("CHOOSE ANY ONE OF THE OPTIONS: ");
+			
+			int options = sc.nextInt();
+			
+			if(options == 1)
+			{
+				System.out.println("1.NEEYUM NAANUM ANBEY");
+				System.out.println("2.VELICHA POOVEY");
+				System.out.println("3.POO INDRU NEEYAGA");
+				System.out.println("4.NEE PARTHA VIZHIGAL");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST: ");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("NEEYUM NAANUM ANBEY....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("VELICHA POOVEY....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("POO INDRU NEEYAGA....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("NEE PARTHA VIZHIGAL....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			}
+			
+			else if(options == 2)
+			{
+				System.out.println("1.VAATHI RAID");
+				System.out.println("2.NAA READY");
+				System.out.println("3.BADAAS");
+				System.out.println("4.VANDHA EDAM");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("VAATHI RAID....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("NAA READY....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("BADAAS....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("VANDHA EDAM....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			}
+			else if(options == 3)
+			{
+				System.out.println("1.VELAI ILLA PATTATHARI");
+				System.out.println("2.BOOMI ENNA SUTHUDHE");
+				System.out.println("3.ETHIR NEECHAL");
+				System.out.println("4.SURVIVA");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+					if(play == 1)
+				{
+					System.out.println("VELAI ILLA PATTATHARI....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("BOOMI ENNA SUTHUDHE....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("ETHIR NEECHAL....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("SURVIVA....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+				
+			}
+			else if(options == 4)
+			{
+				System.out.println("1.PORKANDA SINGAM");
+				System.out.println("2.KANAVE KANAVE");
+				System.out.println("3.KANNNAANA KANNNE");
+				System.out.println("4.JODI NILAVE");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+					if(play == 1)
+				{
+					System.out.println("PORKANDA SINGAM....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("KANAVE KANAVE....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("KANNANA KANNE....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("JODI NILAVE....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			} 
+
+		}
+		else if(singer == 3)//HARRIS JEYARAJ
+		{
+			System.out.println("1.MELODY");
+			System.out.println("2.KUTHU");
+			System.out.println("3.SAD");
+			System.out.print("CHOOSE ANY ONE OF THE OPTIONS: ");
+			
+			int options = sc.nextInt();
+			
+			if(options == 1)
+			{
+				System.out.println("1.UNNALE");
+				System.out.println("2.ANNUL MAELE");
+				System.out.println("3.YEDHO ONDRU");
+				System.out.println("4.VASEEGARA");
+				System.out.println("5.PAARTHA MUDHAL");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("UNNALE....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("ANNUL MAELE....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("YEDHO ONDRU....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("VASEEGARA....");
+				}
+				else if(play == 5)
+				{
+					System.out.println("PAARTHA MUDHAL....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			}
+			
+			else if(options == 2)
+			{
+				System.out.println("1.TIRUNELVELI HALWA DA");
+				System.out.println("2.ARADI KATRE");
+				System.out.println("3.SODA BOTTLE");
+				System.out.println("4.ARUVA MEESAI");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("TIRUNELVELI HALWA DA....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("ARADI KATRE....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("SODA BOTTLE....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("ARUVA MEESAI....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			}
+			else if(options == 3)
+			{
+				System.out.println("1.VENMATHIYE");
+				System.out.println("2.OH MANAME");
+				System.out.println("3.YAMMA YAMMA");
+				System.out.println("4.OTHAYILE");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("VENMATHIYE....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("OH MANAME....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("YAMMA YAMMA....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("OTHAYILE....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+				
+			}
+		}
+		else if(singer == 4) //ILLAIYA RAJA
+		{
+			System.out.println("1.MELODY");
+			System.out.println("2.KUTHU");
+			System.out.println("3.SAD");
+			System.out.print("CHOOSE ANY ONE OF THE OPTIONS: ");
+			
+			int options = sc.nextInt();
+			
+			if(options == 1)
+			{
+				System.out.println("1.ORU KILI URUGUTHU");
+				System.out.println("2.KUZHLOODHUM KANNANUKKU");
+				System.out.println("3.ENNAI THOTTU");
+				System.out.println("4.RAJA RAJA CHOLAN");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("ORU KILI URUGUTHU....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("KUZHLOODUM KANNANUKKU....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("ENNAI THOTTU....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("RAJA RAJA CHOLAN....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}	
+			}
+			
+			else if(options == 2)
+			{
+				System.out.println("1.OORUVITTU ORRUVANTHU");
+				System.out.println("2.NOORU VARUSHAM");
+				System.out.println("3.AASAI NOORUVAGAI");
+				System.out.println("4.AASAI ATHIGAM VACHU");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("OORUVITTU OORUVANTHU....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("NOORU VARUSHAM....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("AASAI NOORUVAGAI....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("AASAI ATHIGAM VACHU....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			}
+			else if(options == 3)
+			{
+				System.out.println("1.KANNE KALAIMANE");
+				System.out.println("2.THENE THENPAANDI NEEYE");
+				System.out.println("3.PAADI PARANTHA KILLI");
+				System.out.println("4.NILAVE VAA");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("KANNE KALAIMANE....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("THENE THENPAANDI NEEYE....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("PAADI PARANTHA KILLI....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("NILAVE VAA....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+				
+			}
+			
+		}
+		else if(singer == 5) // U1
+		{
+			System.out.println("1.MELODY");
+			System.out.println("2.KUTHU");
+			System.out.println("3.MOTIVATIONAL SONG");
+			System.out.println("4.SAD");
+			System.out.print("CHOOSE ANY ONE OF THE OPTIONS: ");
+			
+			int options = sc.nextInt();
+			
+			if(options == 1)
+			{
+				System.out.println("1.AATHADI MANASUDHAN");
+				System.out.println("2.CHINNA SIRISU");
+				System.out.println("3.VENMEGAM");
+				System.out.println("4.IAYYAYOO");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("AATHADI MANASUDHAN....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("CHINNA SIRISU....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("VENMEGAM....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("IAYYAYOO....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			}
+			
+			else if(options == 2)
+			{
+				System.out.println("1.MACHI OPEN THE BOTTLE");
+				System.out.println("2.SAROJA SAMAN NIKALO");
+				System.out.println("3.NO MONEY NO HONEY");
+				System.out.println("4.VILLAYADU MANKATHA");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("MACHI OPEN THE BOTTLE....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("SAROJA SAMAN NIKALO....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("NO MONEY NO HONEY....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("VILLAYADU MANKATHA....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+			}
+			else if(options == 3)
+			{
+				System.out.println("1.EDHIRTHU NILL");
+				System.out.println("2.ORU NALIL");
+				System.out.println("3.NIMIRNTHU NIL");
+				System.out.println("4.UNGAKKULE MIRUGAM");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("EDHIRTHU NILL....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("ORU NALIL....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("NIMIRNTHU NIL....");
+				}
+				else if(play == 4)
+				{
+					System.out.println("UNGAKKULE MIRUGAM....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+				
+			}
+			else if(options == 4)
+			{
+				System.out.println("1.KADHAL VALARTHEN");
+				System.out.println("2.YEDHO ONDRU ENNNAI");
+				System.out.println("3.POGATHEY");
+				System.out.println("4.NINAITHU NINAITHU");
+				
+				System.out.print("CHOOSE FROM ABOVE LIST :");
+				
+					int play = sc.nextInt();
+					
+				if(play == 1)
+				{
+					System.out.println("KADHAL VALARTHEN....");
+				}
+				else if(play == 2)
+				{
+					System.out.println("YEDHO ONDRU ENNAI....");
+				}
+				else if(play == 3)
+				{
+					System.out.println("POGATHEY...");
+				}
+				else if(play == 4)
+				{
+					System.out.println("NINAITHU NINAITHU....");
+				}
+				else
+				{
+			System.out.println("CHOOSE THE SONG WITHIN THE OPTIONS ONLY!!!!");
+				}
+				
+				} 
+		}
+		else
+		{
+			System.out.println("CHOOSE THE SINGER WITHIN THE OPTIONS ONLY!!!!");
+		}
+	}
+}
+
+

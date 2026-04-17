a05ea06394031bb9a1a5f079519b40a034b0bb2d
ArunaChandrasekaran <arunadevitvr@gmail.com>
2026-04-17 13:37:28 +0530

    April17,2026 HOMEWORK

---
+++ HW-17-04-26/PatternHomework.java
@@ -0,0 +1,28 @@
+class PatternHomework
+{
+	public static void main(String args[])
+	{
+		
+		int num = 1;
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(num+ " ");
+				num++;
+			}
+			System.out.println();
+		}
+		
+		char ch = 'A';
+		for(int i=1; i<=5; i++)
+		{
+			for(int j=1; j<=i; j++)
+			{
+				System.out.print(ch+ " ");
+				ch++;
+			}
+			System.out.println();
+		}
+	}
+}

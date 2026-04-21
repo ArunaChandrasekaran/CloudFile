ec48af37bc3cd374af3a8d89e10581fa64ce84b3
ArunaChandrasekaran <arunadevitvr@gmail.com>
2026-04-21 13:46:44 +0530

    April21,2026 home work

---
+++ HW-21-04-26/MultidimentionalArray.java
@@ -0,0 +1,45 @@
+class MultidimentionalArray
+{
+	public static void main(String args[])
+	{
+		int arr1[][] = {{1,2},{3,4}};
+		int arr2[][] = {{5,6},{7,8}};
+		
+		int arr3 [][] = new int[arr1.length][arr1.length];
+		
+		for(int i=0; i<arr1.length; i++)
+		{
+			for(int j=0; j<arr1[i].length; j++)
+			{
+				arr3[i][j]=arr1[i][j]+arr2[i][j];
+			}
+		}
+		
+		System.out.println("ADDING TWO 2D ARRAY AS RESULT IS BELOW:");
+		
+		for(int i=0; i<arr3.length; i++)
+		{
+			for(int j=0; j<arr3[i].length; j++)
+			{
+				System.out.print(arr3[i][j]+" ");
+			}
+			System.out.println();
+		}
+		
+		int findLargest[][] = {{1,8,3},{4,2,6}};
+		int largest=findLargest[0][0];
+		
+		for(int i=0; i<arr3.length; i++)
+		{
+			for(int j=0; j<arr3[i].length; j++)
+			{
+			     if(findLargest[i][j]>largest)
+				 {
+					 largest = findLargest[i][j];
+				 }
+			}
+			System.out.println();
+		}
+		System.out.println(largest);
+	}
+}

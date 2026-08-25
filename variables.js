8b4dc54046c6c9bc4c83f7f99f8bf27d06cc2a83
ArunaChandrasekaran <arunadevitvr@gmail.com>
2026-08-25 14:55:12 +0530

    homework completed

---
+++ variables.js
@@ -0,0 +1,22 @@
+let name = "Arun";
+let age = 20;
+let city = "Madurai";
+
+console.log(name, age, city);
+
+let a = 10;
+let b = 5;
+
+let sum = a + b;
+
+console.log("Sum =", sum);
+
+if (true) {
+    var a = 10;
+    let b = 20;
+    const c = 30;
+}
+
+console.log(a); 
+console.log(b); 
+console.log(c); 

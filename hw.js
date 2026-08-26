8c9c0c7acf89c5dd20a3003a946adf057029fcbe
ArunaChandrasekaran <arunadevitvr@gmail.com>
2026-08-26 22:10:56 +0530

    hom

---
+++ hw.js
@@ -0,0 +1,25 @@
+let arr = [10, 20, 30, 40, 50];
+
+let reverse = [];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    reverse.push(arr[i]);
+}
+
+console.log(reverse);
+
+let arr1 = [10, 20, 30, 40, 50];
+
+arr.reverse();
+
+console.log(arr);
+
+let arr = [10, 20, 30, 40, 50];
+
+let reverse = [];
+
+for (let i = arr.length - 1; i >= 0; i--) {
+    reverse.push(arr[i]);
+}
+
+console.log(reverse);

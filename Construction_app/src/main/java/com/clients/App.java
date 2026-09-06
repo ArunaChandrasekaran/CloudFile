78316202b3d3dcbd0449431fd4e22d5abee1109f
ArunaChandrasekaran <arunadevitvr@gmail.com>
2026-09-07 05:11:27 +0530

    homework

---
+++ Construction_app/src/main/java/com/clients/App.java
@@ -0,0 +1,19 @@
+package com.clients;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        int a = 10;
+        int b = 20;
+
+        int result = a + b;
+
+        System.out.println("First number: " + a);
+        System.out.println("Second number: " + b);
+        System.out.println("Sum: " + result);
+    }
+}

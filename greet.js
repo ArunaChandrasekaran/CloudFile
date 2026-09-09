09678ff8b4c8c54ca92a27a24e9bcccc1185f4a1
ArunaChandrasekaran <arunadevitvr@gmail.com>
2026-09-09 23:12:30 +0530

    homework

---
+++ greet.js
@@ -0,0 +1,19 @@
+
+class Person {
+  
+    constructor(name, age) {
+        
+        this.name = name;
+
+
+        this.age = age;
+    }
+
+   
+    greet() {
+        console.log(`Hello, my name is ${this.name} and I am ${this.age} years old`);
+    }
+}
+
+const person = new Person("Aruna", 25);
+person.greet();
